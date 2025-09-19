69301c0a164246744c60b648804af2f502d6a3a0
MARUTHI-PRASANNA-456 <maruthipr456@gmail.com>
2025-09-19 13:27:21 +0530

    09-monaco-editor-part-1

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -12,24 +12,53 @@ import {
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
 import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
+import { set } from "zod";
+import { useEffect } from "react";
+import {
+  TemplateFile,
+  TemplateFolder,
+} from "@/modules/playground/lib/path-to-json";
 
 const MainPlaygroundPage = () => {
   const { id } = useParams<{ id: string }>();
 
   const { playgroundData, templateData, isLoading, error, saveTemplateData } =
     usePlayground(id);
+  const {
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+    activeFileId,
+    closeAllFiles,
+    openFile,
+    openFiles,
+  } = useFileExplorer();
 
-  console.log("templateData:", templateData);
-  console.log("playgroundData:", playgroundData);
+  useEffect(() => {
+    setPlaygroundId(id);
+  }, [id, setPlaygroundId]);
 
-  const activeFile = "sample.txt"
+  useEffect(() => {
+    if (templateData && !openFiles.length) {
+      setTemplateData(templateData);
+    }
+  }, [templateData, setTemplateData, openFiles.length]);
+
+  const activeFile = openFiles.find((file) => file.id === activeFileId);
+  const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
+
+  const handleFileSelect = (file: TemplateFile) => {
+    openFile(file);
+  };
 
   return (
     <TooltipProvider>
       <>
         <TemplateFileTree
           data={templateData!}
-          onFileSelect={() => {}}
+          onFileSelect={handleFileSelect}
           selectedFile={activeFile}
           title="File Explorer"
           onAddFile={() => {}}
